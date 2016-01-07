f92499770d6c1cf01a3799d21f7e75e0da3438fd
nancy <nancy@DESKTOP-NEC02AJ>
2016-01-07 14:44:31 -0800

    concat

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $name = 'Nancy';
+        $what = 'geek';
+        $level = 10;
+        echo 'Hi, my name is '.$name.'. And I am a level '.$level.' '.$what;
+        ?>
+    </body>
+</html>
