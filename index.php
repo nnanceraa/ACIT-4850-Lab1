50ee9061236fd1202548f777de661ce461932669
nancy <nancy@DESKTOP-NEC02AJ>
2016-01-09 20:29:19 -0800

    tic tac toe done

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -12,42 +7,150 @@ and open the template in the editor.
     <body>
         <?php
         
-        if (winner('x', $square)) {
-            echo 'You win.';
-        } else if (winner('o', $square)) {
-            echo 'I win.';
-        } else {
-            echo 'No winner yet';
+        //takes the string from URL
+        $squares = $_GET['board'];
+        
+        //creating a new game
+        $game =new Game($squares);
+        
+        //calling on the pick_move method
+        $game->pick_move();
+
+        //calling on the display method
+        $game->display();
+        
+        //if the winner method returns x then player wins
+        if ($game->winner('x')){
+            echo 'Beginners luck, you won this round.';
         }
+        //if winner method returns o then bot wins
+        else if ($game->winner('o')){
+            echo 'I win. Better luck next time loser!';
+        } 
+        //if other conditions are met then no winner
+        else {
+            echo 'No winner yet, but I\'m pretty much winning.';
+        }
+        
         ?>
+        
     </body>
 </html>
-<?php
-        // put your code here
-       $position = $_GET['board'];
-       $square = str_split($postion);
-       
+<?php   
+      
+class Game {
+    
+    var $position;              //current position in the array 
+    
+    //constructor which takes in the $squares 
+    function __construct($squares) {
+        //assigning $squares to the variable
+        $this->position = str_split($squares);
+    }
        //WINNING PATTERNS FUNCTION
-       function winner ($tocken, $position){
+       function winner ($token){
+       //return false if none of the conditions are met
        $won = false;
-       if (($position[0] == $tocken) && ($position[1]== $tocken) && ($position[2] == $tocken)){
+       
+       //top row
+       if (($this->position[0] == $token) && ($this->position[1]== $token) && ($this->position[2] == $token)){
            $won = true;
-       } else if (($position[3] == $tocken) && ($position[4]== $tocken) && ($position[5] == $tocken)){
-            $won = true;
-       } else if (($position[6] == $tocken) && ($position[7]== $tocken) && ($position[8] == $tocken)){
-            $won = true;
-       } else if (($position[0] == $tocken) && ($position[4]== $tocken) && ($position[8] == $tocken)){
-            $won = true;
-       } else if (($position[2] == $tocken) && ($position[4]== $tocken) && ($position[6] == $tocken)){
-            $won = true;
-       } else if (($position[1] == $tocken) && ($position[4]== $tocken) && ($position[7] == $tocken)){
-            $won = true;
-       } else if (($position[0] == $tocken) && ($position[3]== $tocken) && ($position[6] == $tocken)){
-            $won = true;
-       } else if (($position[2] == $tocken) && ($position[5]== $tocken) && ($position[8] == $tocken)){
-            $won = true;
        }
-       }
+       //middle row
+       else if (($this->position[3] == $token) && ($this->position[4]== $token) && ($this->position[5] == $token)){
+            $won = true;
+       } 
+       //bottom row
+       else if (($this->position[6] == $token) && ($this->position[7]== $token) && ($this->position[8] == $token)){
+            $won = true;
+       } 
+       //left column
+       else if (($this->position[0] == $token) && ($this->position[4]== $token) && ($this->position[8] == $token)){
+            $won = true;
+       } 
+       //right column
+       else if (($this->position[2] == $token) && ($this->position[4]== $token) && ($this->position[6] == $token)){
+            $won = true;
+       } 
+       //middle column
+       else if (($this->position[1] == $token) && ($this->position[4]== $token) && ($this->position[7] == $token)){
+            $won = true;
+       } 
+       //first diagonl
+       else if (($this->position[0] == $token) && ($this->position[3]== $token) && ($this->position[6] == $token)){
+            $won = true;
+       } 
+       //second diagonal
+       else if (($this->position[2] == $token) && ($this->position[5]== $token) && ($this->position[8] == $token)){
+            $won = true;
+       } 
+       return $won;
+       } 
        
+       //DISPLAYS THE A TABLE OF 9 BLOCKS
+       function display(){
+        echo '<h2>Welcome to Nancy\'s Tic Tac Toe </h2>';
+        
+        echo '<form action=>';
+            echo '<div>';
+                echo '<input type="hidden" name="board" value="---------"/>';
+		echo '<input type="submit" name="new" value="New Game"/>';
+            echo '</div>';
+        echo '</form> </br>';
+            echo '<table  border= "1" cols="3" style="font-size:50px; font-weight:bold">';
+            echo '<tr>';
+            
+            //for loop iterates 8 times to create each position
+            for ($pos=0; $pos <9;$pos++){
+                echo $this->show_cell($pos);
+                //end and create new row
+                if ($pos %3 == 2){
+                    echo '</tr><tr>';
+                }
+            }
+            echo '<tr>';
+            echo '</table>  </br>';
+        }
+        
+       //function will show either '-' or the token 'x' or 'o'
+       function show_cell($which) {
+        $token = $this->position[$which];
+        
+        //display token inside the box if it isnt '-'
+        if ($token <> '-') 
+            return '<td>'.$token.'</td>';
+        
+        //making a copy of the position
+        $this->newposition = $this->position;
+        
+        //this new position will be opponent 'x' turn
+        $this->newposition[$which] = 'x';
+        
+        //adding o to the board array
+        $move = implode($this->newposition);
+
+        //adding to the link
+        $link = '?board='.$move;
+        
+        //return a cell showing a '-'
+        return '<td><a style="text-decoration: none;" href="'.$link.'">-</a></td>';
+        }
        
-        ?>
+        //function looks for the next available slot to place an o in
+        function pick_move() { 
+            
+            //iterate through all the positions
+            for($i=0;$i<8;$i++){
+                
+                //checking for an empty slot aka hyphens then replace with o
+                if($this->position[$i] == '-'){
+                    $this->position[$i] = 'o';
+                    break;
+                    } 
+           
+            }
+            }
+        
+     }   
+       
+ ?>
